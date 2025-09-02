55a30d63af7a0e9b8200aca3c86bf39984b5da1c
steehhh <stefanypradebon@gmail.com>
2025-09-02 16:01:46 -0300

    aa

--- src/componentes/QuemSou/QuemSou.js
+++ src/componentes/QuemSou/QuemSou.js
@@ -2,17 +2,17 @@ import './QuemSou.css'
 
 function QuemSou() {
     return (
-        <div className='Sobre'>
-            <h1>Sobre<br />meu trabalho</h1>
-            <p>Na nossa clínica,
-                oferecemos serviços de psicologia fundamentados
-                em rigorosos padrões científicos,
-                buscando promover a saúde mental
-                e o bem-estar emocional de nossos clientes.
-                Contamos com uma equipe altamente qualificada,
-                dedicada a proporcionar atendimentos personalizados
-                para cada necessidade.</p>
-        </div>
+        <section className="Sobre">
+            <h1>Mais sobre <br /> nossa marca</h1>
+            <div className="paragrafo-container">
+                <p>
+                    Na nossa clínica,<br />oferecemos serviços de psicologia fundamentados<br/> em rigorosos padrões científicos,  <br />
+                    buscando promover a saúde mental <br />e o bem-estar emocional de nossos clientes. <br /> Contamos com uma equipe altamente qualificada, <br />
+                    dedicada a proporcionar atendimentos personalizados<br/> para cada necessidade.
+                </p>
+            </div>
+        </section>
+
     )
 }
 
