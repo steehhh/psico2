c67cff2ee058b5960197b428f21942798f9c2fa2
steehhh <stefanypradebon@gmail.com>
2025-09-03 20:00:08 -0300

    redessociais

---
+++ src/componentes/RedesSociais/RedesSociais.js
@@ -0,0 +1,22 @@
+import './RedesSociais.css'
+
+function RedesSociais () {
+    return(
+        <section className='redes-sociais'>
+            <h2 className='redes-sociais__titulo'>Siga-nos nas redes sociais</h2>
+            <ul className='redes-sociais__lista'>
+                <li className='redes-sociais__item'>
+                    <a  href="https://wa.me/55999341346" target='_blank' rel="noopener noreferrer" className='redes-sociais__link'>
+                        <img src="/iconeWhatsApp.png" alt="WhatsApp" />
+                    </a>
+                </li>
+            <li className='redes-sociais__item2'>
+                <a href='https://www.instagram.com/nataliakpsi/' target='_blank' rel="noopener noreferrer" className='redes-sociais__link2' >
+                     <img src="/iconeInstagram.png" alt="Instagram" />
+                </a>
+            </li>
+            </ul>
+        </section>
+    )
+}
+export default RedesSociais
