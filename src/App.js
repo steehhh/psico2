3e161f87a2e72ff3097ca06bb1c35511c1cae81c
steehhh <stefanypradebon@gmail.com>
2025-09-09 19:22:01 -0300

    termino

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import RedesSociais from './componentes/RedesSociais/RedesSociais'
 import Servicos from './componentes/Servicos/Servicos'
 import Agendamento from './componentes/Agendamento/Agendamento'
 import Footer from './componentes/Footer/Footer'
+import Background from './componentes/Background/Background';
 
 function App() {
   return (
     <div className="App">
+      <Background />
       <Header />
       <Hero />
       <QuemSou />
--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -1,16 +1,34 @@
 import './Header.css'
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Header() {
+
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const isScrolled = window.scrollY > 50;
+            if (isScrolled !== scrolled) {
+                setScrolled(isScrolled);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [scrolled]);
+
+    const headerClass = `header ${scrolled ? 'scrolled' : ''}`;
+
     return (
-        <header className="header">
+        <header className={headerClass}>
             <nav className="header_nav">
                 <ul className="header__nav_list">
                     <li className="header__nav_list_item">
                         <a className="header__nav_list_link" href="#Hero">Natalia Kieling...</a>
                     </li>
-
-                    {/* Novo grupo de links */}
                     <div className="header__links_group">
                         <li className="header__nav_list_item2">
                             <a className="header__nav_list_link2" href="#Servicos">Serviços</a>
